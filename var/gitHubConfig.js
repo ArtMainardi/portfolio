8092b265d6ed47eeb1544e15b6489439249aaf04
ArtMainardi <iensenmainardi@gmail.com>
2026-07-01 21:25:48 -0300

    fix: corrige aplicação do 'gitHubConfig.js'

--- var/gitHubConfig.js
+++ var/gitHubConfig.js
@@ -1,6 +1,6 @@
 let html = "";
 if (window.location.hostname.includes('github.io')) {
-    html = `<base id="base-url" href="/porfolio/">`
+    html = `<base id="base-url" href="/portfolio/">`
 }
 
 html += document.querySelector("head").innerHTML;
